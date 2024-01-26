deac5cc9b99bc87cf187367852748d7779e2c9af
Tri Fajar Pangestu <tf_png@Tris-MacBook-Pro.local>
2024-01-26 10:04:12 +0700

    first preview

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { useDataPlayer } from "@/hooks/useDataPlayer"
 import { DataStream } from "@/utils/types"
 import Head from "next/head"
 import { useMemo } from "react"
+import { FaPlay } from "react-icons/fa"
 import { useQuery } from "react-query"
 
 export default function Home() {
@@ -16,16 +17,19 @@ export default function Home() {
       <Head>
         <title>Streaming Radio Nusantara</title>
       </Head>
-      <div className=" grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-2 p-10">
+      <div className=" grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 p-10">
         {items?.map((v, i: number) => (
-          <div key={i} onClick={() => onChangeRadio(v)}>
-            <div className=" bg-white w-[80px] h-[80px] md:w-[130px] md:h-[130px] lg:w-[180px] lg:h-[180px] rounded-md overflow-hidden">
+          <div key={i} onClick={() => onChangeRadio(v)} className=" flex flex-col items-center text-center">
+            <div className="group bg-white w-[80px] h-[80px] md:w-[130px] md:h-[130px] lg:w-[180px] lg:h-[180px] rounded-md overflow-hidden relative">
+              <div className="absolute bg-[rgba(0,0,0,.5)] w-full h-full group-hover:grid justify-center items-center hidden duration-75 cursor-pointer">
+                <FaPlay className=" size-8" />
+              </div>
               <img
                 src={v.logo || "https://www.generationsforpeace.org/wp-content/uploads/2018/03/empty.jpg"} alt={v.title}
                 className="w-[100%] h-[100%] object-contain"
               />
             </div>
-            <span>{v.title}</span>
+            <div className=" mt-4">{v.title}</div>
           </div>
         ))}
       </div>
